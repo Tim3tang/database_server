54b0fac88ac5529de0406361795c601bde80df59
Tim3tang <Tim3tang@gmail.com>
2016-10-17 15:34:56 +0100

    added requirements.txt

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import unittest
 from app import app
 
+
 class DbServer(unittest.TestCase):
 
     def test_index(self):
@@ -8,6 +9,12 @@ class DbServer(unittest.TestCase):
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
+    def test_set_get(self):
+        set_response = requests.get(BASE_URL + '/set?key1=val1')
+        self.assertEqual(200, set_response.status_code)
+        get_response = requests.get(BASE_URL + '/get?key=key1')
+        self.assertEqual(200, get_response.status_code)
+        self.assertEqual('val1', get_response.text)
 
 if __name__ == '__main__':
     unittest.main()
